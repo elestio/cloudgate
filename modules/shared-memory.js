aaab9cb99d4167a9090d4ac9725669a1fd8c6a7c
jbenguira <joseph@appdrag.com>
2020-08-10 19:29:53 +0200

    .

--- modules/shared-memory.js
+++ modules/shared-memory.js
@@ -2,40 +2,40 @@ const sharedmem = require('uWebSockets.js');
 module.exports = {
   getString: function(key, collection){
       sharedmem.lock();
-      var tmp = sharedmem.getString(key + "", collection);
+      var tmp = sharedmem.getString(key + "", collection + "");
       sharedmem.unlock();
       return tmp;
   },
   setString: function(key, value, collection){
       sharedmem.lock();
-      sharedmem.setString(key + "", value + "", collection);
+      sharedmem.setString(key + "", value + "", collection + "");
       sharedmem.unlock();
   },
   getInteger: function(key, collection){
       sharedmem.lock();
-      var tmp = sharedmem.getInteger(key + "", collection);
+      var tmp = sharedmem.getInteger(key + "", collection + "");
       sharedmem.unlock();
       return tmp;
   },
   setInteger: function(key, value, collection){
       sharedmem.lock();
-      sharedmem.setInteger(key + "", value, collection);
+      sharedmem.setInteger(key + "", value + "", collection + "");
       sharedmem.unlock();
   },
   incInteger: function(key, value, collection){
       sharedmem.lock();
-      sharedmem.incInteger(key + "", value, collection);
+      sharedmem.incInteger(key + "", value + "", collection + "");
       sharedmem.unlock();
   },
   getStringKeys: function(collection){
       sharedmem.lock();
-      var tmp = sharedmem.getStringKeys(collection);
+      var tmp = sharedmem.getStringKeys(collection + "");
       sharedmem.unlock();
       return tmp;
   },
   getIntegerKeys: function(collection){
       sharedmem.lock();
-      var tmp = sharedmem.getIntegerKeys(collection);
+      var tmp = sharedmem.getIntegerKeys(collection + "");
       sharedmem.unlock();
       return tmp;
   }

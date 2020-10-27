b60c01ba7e8259eee193a3ea6a70c89024cb774e
jbenguira <joseph@appdrag.com>
2020-10-27 14:31:14 +0100

    fix to unattended script

--- systemd/install_unattended.sh
+++ systemd/install_unattended.sh
@@ -2,7 +2,7 @@ echo "";
 echo "Installing Cloudgate as a service with SystemD ..."
 
 ##TCP PORT (default to port 80)
-uPort=80
+uPort=9000
 
 ##Threads/Cores (default: all cores available)
 nbCores=$(grep -c processor /proc/cpuinfo)
@@ -27,7 +27,7 @@ echo "Copying configuration: ......";
 mkdir -p /etc/cloudgate/
 mkdir -p $APP_ROOT
 #cp cloudgate.service /etc/systemd/system/cloudgate.service
-wget https://raw.githubusercontent.com/elestio/cloudgate/master/systemd/cloudgate.service
+wget -O /etc/systemd/system/cloudgate.service https://raw.githubusercontent.com/elestio/cloudgate/master/systemd/cloudgate.service
 sed -i "s#root#${userName}#g" /etc/systemd/system/cloudgate.service
 sed -i "s#binpath#${cgPath}#g" /etc/systemd/system/cloudgate.service
 
@@ -41,7 +41,25 @@ echo "";
 
 #copy default files in approot
 mkdir -p $APP_ROOT/default
-cp -r ../apps/CatchAll/*  $APP_ROOT/default
+#cp -r ../apps/CatchAll/*  $APP_ROOT/default
+
+mkdir -p $APP_ROOT/default/public
+mkdir -p $APP_ROOT/default/public/css
+mkdir -p $APP_ROOT/default/API
+mkdir -p $APP_ROOT/default/API/tests
+mkdir -p $APP_ROOT/default/API/websocket
+
+wget -O $APP_ROOT/default/appconfig.json https://raw.githubusercontent.com/elestio/cloudgate/master/apps/CatchAll/appconfig.json
+wget -O $APP_ROOT/default/API/tests/simple.js https://raw.githubusercontent.com/elestio/cloudgate/master/apps/CatchAll/API/tests/simple.js
+wget -O $APP_ROOT/default/API/tests/full.js https://raw.githubusercontent.com/elestio/cloudgate/master/apps/CatchAll/API/tests/full.js
+wget -O $APP_ROOT/default/API/websocket/Echo.js https://raw.githubusercontent.com/elestio/cloudgate/master/apps/CatchAll/API/websocket/Echo.js
+wget -O $APP_ROOT/default/API/websocket/Chat.js https://raw.githubusercontent.com/elestio/cloudgate/master/apps/CatchAll/API/websocket/Chat.js
+wget -O $APP_ROOT/default/index.html https://raw.githubusercontent.com/elestio/cloudgate/master/apps/CatchAll/public/index.html
+wget -O $APP_ROOT/default/css/main.css https://raw.githubusercontent.com/elestio/cloudgate/master/apps/CatchAll/public/css/main.css
+wget -O $APP_ROOT/default/wsChat.html https://raw.githubusercontent.com/elestio/cloudgate/master/apps/CatchAll/public/wsChat.html
+wget -O $APP_ROOT/default/wsDemo.html https://raw.githubusercontent.com/elestio/cloudgate/master/apps/CatchAll/public/wsDemo.html
+wget -O $APP_ROOT/default/wsAdmin.html https://raw.githubusercontent.com/elestio/cloudgate/master/apps/CatchAll/public/wsAdmin.html
+wget -O $APP_ROOT/default/wsEcho.html https://raw.githubusercontent.com/elestio/cloudgate/master/apps/CatchAll/public/wsEcho.html
 
 systemctl enable cloudgate
 systemctl daemon-reload

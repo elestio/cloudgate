aeef8de3bd11e56aa7dd1544177fd8e478dbcb7e
jbenguira <joseph@appdrag.com>
2020-11-12 18:52:04 +0100

    fix

--- apps/MYSQL/DB/MYSQL/startMYSQL_unattended.sh
+++ apps/MYSQL/DB/MYSQL/startMYSQL_unattended.sh
@@ -2,10 +2,10 @@
 main() {
     
     #UNATTENDED
-    NETFACE=$1; #172.17.0.1
-    NETPORT=$2; #3306
-    DBPATH=$3; #$PWD/data
-    DBNAME=$4; #MyDB1
+    NETFACE=172.17.0.1; #172.17.0.1
+    NETPORT=3306; #3306
+    DBPATH=$PWD/data; #$PWD/data
+    DBNAME=myapp; #MyDB1
 
     #IF the data folder is not empty, root password will be the old one ...
     if emptydir $DBPATH

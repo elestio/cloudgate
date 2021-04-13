d2d1effc55ad29c1bb5fb0f472cbf7219ffc443e
jbenguira <joseph@appdrag.com>
2021-04-13 07:48:58 +0200

    fix to Dynamic datasource

--- modules/dynamic-datasource.js
+++ modules/dynamic-datasource.js
@@ -146,6 +146,10 @@ module.exports = async (responseToProcess, queryStringParams, appConfig, reqInfo
                 }
 
                 var rows = result.content.Table;
+                if ( typeof result.content == "string" ){
+                    rows = JSON.parse(result.content).Table;
+                }
+                
                 //console.log(rows)
                 //console.log(newReqInfos.url + "  -  " + rows.length);
                 //var rows = [];

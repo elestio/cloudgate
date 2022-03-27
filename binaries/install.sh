82da7ddde1de2a378dafa6b8d6bc743772122c37
jbenguira <joseph@appdrag.com>
2022-03-27 14:35:05 +0200

    bin

--- binaries/install.sh
+++
@@ -1,1 +0,0 @@
-wget -c https://raw.githubusercontent.com/elestio/cloudgate/master/binaries/cloudgate-linux.tar.gz && tar -xzf cloudgate-linux.tar.gz -C /bin && rm cloudgate-linux.tar.gz
---
+++ installBin.sh
@@ -0,0 +1,1 @@
+wget -c https://raw.githubusercontent.com/elestio/cloudgate/master/binaries/cloudgate-linux.tar.gz && tar -xzf cloudgate-linux.tar.gz -C /bin && rm cloudgate-linux.tar.gz
